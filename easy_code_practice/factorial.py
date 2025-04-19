df030569c880d2d78163885a68ed7ed0b4ff543e
Ruchika Mahajan <ruchikamahajan66@gmail.com>
2025-04-19 16:10:37 -0700

    factorial of a given number

---
+++ easy_code_practice/factorial.py
@@ -0,0 +1,29 @@
+"""
+iterative solution
+with time complexity = O(n)
+"""
+
+
+def factorial_of_a_number(num, fact):
+    for i in range(2, num + 1):
+        fact = fact * i
+    return fact
+
+
+"""
+iterative solution
+with time complexity = O(n)
+"""
+
+
+def factorial_of_a_number_recurssion(num, fact):
+    for i in range(2, num + 1):
+        fact = fact * i
+    return fact
+
+
+if __name__ == '__main__':
+    number = 5
+    fact = 1
+    print("iterative factorial is ",factorial_of_a_number(number, fact))
+    print("recursive factorial is ",factorial_of_a_number_recurssion(number, fact))
