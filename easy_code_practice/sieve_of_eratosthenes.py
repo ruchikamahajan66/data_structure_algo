5dae493508fe8f382a167044dcd5a724234b6aab
Ruchika Mahajan <ruchikamahajan66@gmail.com>
2025-04-21 17:05:01 -0700

    sieve_of_eratosthenes_algo

---
+++ easy_code_practice/sieve_of_eratosthenes.py
@@ -0,0 +1,16 @@
+def SieveOfEratosthenes(n):
+    prime = [True for i in range(n + 1)]
+    i = 2
+    while (i * i <= n):
+        if prime[i]:
+            for j in range(i * i, n + 1, i):
+                prime[j] = False
+        i = i + 1
+    for i in range(2, n + 1):
+        if prime[i]:
+            print(i)
+
+
+if __name__ == '__main__':
+    number = 30
+    print(SieveOfEratosthenes(number))
