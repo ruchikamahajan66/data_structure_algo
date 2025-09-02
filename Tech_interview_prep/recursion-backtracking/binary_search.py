b62c7c804b9bb6cbaf6129331cc186eab508b16e
Ruchika Mahajan <ruchikamahajan66@gmail.com>
2025-09-01 21:48:20 -0700

    array practice problems

--- Tech_interview_prep/recursion-backtracking/binary_search.py
+++ Tech_interview_prep/recursion-backtracking/binary_search.py
@@ -11,9 +11,27 @@ def binary_search(arr, start, end, key):
         return binary_search(arr, mid + 1, end, key)
 
 
+def binary_search_iterative_slution(arr, start, end, key):
+    while (start <= end):
+        mid = int((start + end) / 2)
+        present = True
+
+        if arr[mid] == key:
+            print(mid)
+            break
+        if arr[mid] < key:
+            start = mid + 1
+        else:
+            end = mid - 1
+        if not present:
+            print(-1)
+
+
 if __name__ == '__main__':
     arr = [2, 5, 8, 16, 20, 23, 30, 35, 41, 63]
     target = 6
     start = 0
     end = len(arr) - 1
-    print(binary_search(arr, start, end, target))
+    # print(binary_search(arr, start, end, target))
+    print(binary_search_iterative_slution(arr, start, end, target))
+
